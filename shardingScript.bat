ec0e37c4a74d19fddfc7587fe37f6c4b57ff0232
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2020-11-28 14:10:55 +0530

    Add sharding script

---
+++ shardingScript.bat
@@ -0,0 +1,25 @@
+@REM '@REM' is a comment
+
+@REM To create config server replica set folders
+md configServer\configServer1\db configServer\configServer1\log configServer\configServer2\db configServer\configServer2\log configServer\configServer3\db configServer\configServer3\log
+
+@REM To create folders for shard1
+md shard1\shard11\db shard1\shard11\log shard1\shard12\db shard1\shard12\log shard1\shard13\db shard1\shard13\log
+
+@REM To create folders for shard2
+md shard2\shard21\db shard2\shard21\log shard2\shard22\db shard2\shard22\log shard2\shard23\db shard2\shard23\log
+
+@REM To create folders for shard3
+md shard3\shard31\db shard3\shard31\log shard3\shard32\db shard3\shard32\log shard3\shard33\db shard3\shard33\log
+
+@REM To create log for mongos router
+md router\log
+
+
+@REM Open configServer folder
+cd configServer
+
+@REM Start mongod with the configuration
+start mongod -f configServerConfiguration1.conf
+start mongod -f configServerConfiguration2.conf
+start mongod -f configServerConfiguration3.conf
