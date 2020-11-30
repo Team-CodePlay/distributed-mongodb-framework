31617848ec0803ce61de133af4bde21472e8f2b4
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2020-12-01 03:20:01 +0530

    Add cleaning script

---
+++ cleaningScript.sh
@@ -0,0 +1,4 @@
+sudo rm -rf configServer/configServer1 configServer/configServer2 configServer/configServer3
+sudo rm -rf shard1/shard11 shard1/shard12 shard1/shard13
+sudo rm -rf shard2/shard21 shard2/shard22 shard2/shard23
+sudo rm -rf shard3/shard31 shard3/shard32 shard3/shard33
