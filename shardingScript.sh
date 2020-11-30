ee1a6944f1c876369844847ecdf921de321070fa
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2020-12-01 00:36:23 +0530

    Change script extension

---
+++ shardingScript.sh
@@ -0,0 +1,13 @@
+
+mkdir configServer/configServer1/db configServer/configServer1/log configServer/configServer2/db configServer/configServer2/log configServer/configServer3/db configServer/configServer3/log
+
+mkdir shard1/shard11/db shard1/shard11/log shard1/shard12/db shard1/shard12/log shard1/shard13/db shard1/shard13/log
+
+mkdir shard2/shard21/db shard2/shard21/log shard2/shard22/db shard2/shard22/log shard2/shard23/db shard2/shard23/log
+
+mkdir shard3/shard31/db shard3/shard31/log shard3/shard32/db shard3/shard32/log shard3/shard33/db shard3/shard33/log
+
+mkdir router/log
+
+cd configServer
+
