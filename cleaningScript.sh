f6e8b96122eeae86aef3ef10f3a2a66a7efbf111
EC2 Default User <ec2-user@ip-172-31-46-240.ap-south-1.compute.internal>
2020-12-01 09:27:14 +0000

    updated cleaningScript

--- cleaningScript.sh
+++ cleaningScript.sh
@@ -2,3 +2,4 @@ sudo rm -rf configServer/configServer1 configServer/configServer2 configServer/c
 sudo rm -rf shard1/shard11 shard1/shard12 shard1/shard13
 sudo rm -rf shard2/shard21 shard2/shard22 shard2/shard23
 sudo rm -rf shard3/shard31 shard3/shard32 shard3/shard33
+sudo rm -rf router/log
--- runMongosWith2ConfigServer.sh
+++ runMongosWith2ConfigServer.sh
@@ -1,2 +1,2 @@
-configServerDNS=ec2-15-207-248-74.ap-south-1.compute.amazonaws.com
+configServerDNS=ec2-13-233-35-223.ap-south-1.compute.amazonaws.com
 sudo mongos -f router/mongos.conf --configdb configServerReplicationSet/$configServerDNS:26001,$configServerDNS:26002 --fork
