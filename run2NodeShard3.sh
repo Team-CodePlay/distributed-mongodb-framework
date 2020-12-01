b7e0af29395ad77807103b55088592c49599a85d
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2020-12-01 15:36:08 +0530

    Add bind ip to public dns

--- run2NodeShard3.sh
+++ run2NodeShard3.sh
@@ -1,2 +1,2 @@
-sudo mongod -f shard3/shard31Config.conf --fork
-sudo mongod -f shard3/shard32Config.conf --fork
+sudo mongod -f shard3/shard31Config.conf --bind_ip $publicIP --fork
+sudo mongod -f shard3/shard32Config.conf --bind_ip $publicIP --fork
