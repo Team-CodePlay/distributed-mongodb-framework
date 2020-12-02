912d24ed26d639d26a8aa168965a1c39a1167603
HOD101s <manasbass.99@gmail.com>
2020-12-02 21:56:28 +0530

    updated runShard script

--- runShard.sh
+++ runShard.sh
@@ -1,2 +1,3 @@
 sudo mongod -f shard$1/shard$11Config.conf --bind_ip $publicDNS --fork
 sudo mongod -f shard$1/shard$12Config.conf --bind_ip $publicDNS --fork
+sudo mongod -f shard$1/shard$13Config.conf --bind_ip $publicDNS --fork
