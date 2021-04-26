3edcaf735a573bfc0badf53b9f75d8cb8b87a01c
Sanket Dalvi <36796116+sank8dalvi@users.noreply.github.com>
2021-04-26 11:52:30 +0530

    Update runMongosWith2ConfigServer.sh

--- runMongosWith2ConfigServer.sh
+++ runMongosWith2ConfigServer.sh
@@ -1,2 +1,4 @@
-configServerDNS=ec2-13-233-35-223.ap-south-1.compute.amazonaws.com
-sudo mongos -f router/mongos.conf --configdb configServerReplicationSet/$configServerDNS:26001,$configServerDNS:26002 --bind_ip $publicDNS --fork
+configServerDNS1=ec2-13-233-35-223.ap-south-1.compute.amazonaws.com
+configServerDNS2=ec2-13-233-35-223.ap-south-1.compute.amazonaws.com
+configServerDNS3=ec2-13-233-35-223.ap-south-1.compute.amazonaws.com
+sudo mongos -f router/mongos.conf --configdb configServerReplicationSet/$configServerDNS1,$configServerDNS2,$configServerDNS --bind_ip $publicDNS --fork
